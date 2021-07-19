59757fd4ba7b371ba63067549ba656c72d6f6c9a
Nguyên Trần <nguyentranthepog@gmail.com>
2021-07-19 16:59:44 +0700

    feat: add ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,23 @@
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@emotion/react';
+
 import 'normalize.css';
 import GlobalStyles from '@styles/GlobalStyles';
 import Layout from '@components/Layout';
 
+import theme from '@styles/theme';
 import store from 'redux/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <GlobalStyles />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
     </Provider>
   );
 }
