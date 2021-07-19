0d3a8e761afe8c3f75a59f80d86618fbafe77ff4
Nguyên Trần <nguyentranthepog@gmail.com>
2021-07-19 16:57:19 +0700

    feat: add theme

--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -1,33 +1,28 @@
-import { Global, css } from '@emotion/react';
+import { Global, css, useTheme } from '@emotion/react';
 
-const GlobalStyles = () => (
-  <Global
-    styles={css`
-      :root {
-        --color-white: #fff;
-        --color-black: #000;
-        --color-green: #16c784;
-        --color-red: #ea3943;
-        --color-gray: #959595;
-        --color-grayLight: #f5f5f5;
-      }
+const GlobalStyles = () => {
+  const theme = useTheme();
 
-      *,
-      *::before,
-      *::after {
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-      }
+  return (
+    <Global
+      styles={css`
+        *,
+        *::before,
+        *::after {
+          padding: 0;
+          margin: 0;
+          box-sizing: border-box;
+        }
 
-      body {
-        font-family: 'Poppins', sans-serif;
-        font-size: 1rem;
-        color: var(--color-black);
-        background-color: var(--color-grayLight);
-      }
-    `}
-  />
-);
+        body {
+          font-family: 'Poppins', sans-serif;
+          font-size: 1rem;
+          color: ${theme.colors.black};
+          background-color: ${theme.colors.grayLight};
+        }
+      `}
+    />
+  );
+};
 
 export default GlobalStyles;
---
+++ styles/theme.ts
@@ -0,0 +1,16 @@
+const colors = {
+  white: '#fff',
+  black: '#000',
+  green: '#16c784',
+  red: '#ea3943',
+  gray: '#959595',
+  grayLight: '#f5f5f5',
+  borderColor: '#d6d6d6',
+};
+
+const theme = {
+  colors,
+};
+
+export type ColorsType = typeof colors;
+export default theme;
